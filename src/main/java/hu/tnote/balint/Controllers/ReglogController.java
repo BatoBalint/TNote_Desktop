c893b990670a9fb4434525ec2177258139069c5f
BatoBalint <balint.bato@gmail.com>
2022-03-06 14:02:49 +0100

    ui javitas, json-simple dependency hozzaadva

--- src/main/java/hu/tnote/balint/Controllers/ReglogController.java
+++ src/main/java/hu/tnote/balint/Controllers/ReglogController.java
@@ -1,5 +1,11 @@
 package hu.tnote.balint.Controllers;
 
+import javafx.beans.InvalidationListener;
+import javafx.beans.Observable;
+import javafx.beans.binding.BooleanBinding;
+import javafx.beans.property.SimpleBooleanProperty;
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableBooleanValue;
 import javafx.fxml.FXML;
 import javafx.scene.control.*;
 import javafx.scene.layout.HBox;
@@ -30,14 +36,22 @@ public class ReglogController {
     @FXML
     private TextField emailInput;
     @FXML
-    private TextField passwordInput;
+    private PasswordField passwordAgainInputDots;
     @FXML
-    private TextField passwordAgainInput;
+    private TextField passwordAgainInputText;
+    @FXML
+    private PasswordField passwordInputDots;
+    @FXML
+    private TextField passwordInputText;
     //endregion
 
     private boolean regSelected;
+    private SimpleBooleanProperty showPassword;
 
     public void initialize() {
+        showPassword = new SimpleBooleanProperty();
+        showPassword.set(false);
+
         uiInit();
 
         regBtnSelected();
@@ -48,6 +62,19 @@ public class ReglogController {
         HBox.setHgrow(loginBtn, Priority.ALWAYS);
         regBtn.setMaxWidth(Double.MAX_VALUE);
         loginBtn.setMaxWidth(Double.MAX_VALUE);
+
+        passwordInputText.visibleProperty().bind(showPassword);
+        passwordInputText.managedProperty().bind(showPassword);
+        passwordAgainInputText.visibleProperty().bind(showPassword);
+        passwordAgainInputText.managedProperty().bind(showPassword);
+
+        passwordInputDots.visibleProperty().bind(showPassword.not());
+        passwordInputDots.managedProperty().bind(showPassword.not());
+        passwordAgainInputDots.visibleProperty().bind(showPassword.not());
+        passwordAgainInputDots.managedProperty().bind(showPassword.not());
+
+        passwordInputText.textProperty().bindBidirectional(passwordInputDots.textProperty());
+        passwordAgainInputText.textProperty().bindBidirectional(passwordAgainInputDots.textProperty());
     }
 
     @FXML
@@ -62,64 +89,71 @@ public class ReglogController {
 
     @FXML
     public void submitBtnClick() {
-        String text = String.format("Name: %s\nEmail: %s\nPass: %s", nameInput.getText(), emailInput.getText(), passwordInput.getText());
+        //String text = String.format("Name: %s\nEmail: %s\nPass: %s", nameInput.getText(), emailInput.getText(), passwordInputText.getText());
         //alert((regSelected) ? "reg selected" : "login selected");
-        alert(text);
+        //alert(text);
+        showPassword.set(!showPassword.get());
     }
 
+    //mainly styles
     public void regBtnSelected() {
-        submitBtn.setText("Regisztráció");
+        if (!regSelected) {
 
-        nameContainer.setVisible(true);
-        nameContainer.setManaged(true);
-        passwordAgainContainer.setVisible(true);
-        passwordAgainContainer.setManaged(true);
+            submitBtn.setText("Regisztráció");
 
-        regBtn.getStyleClass().removeAll("regBtn");
-        regBtn.getStyleClass().removeAll("roundTRB");
-        regBtn.getStyleClass().removeAll("secondaryTextColor");
+            nameContainer.setVisible(true);
+            nameContainer.setManaged(true);
+            passwordAgainContainer.setVisible(true);
+            passwordAgainContainer.setManaged(true);
 
-        regBtn.getStyleClass().add("regBtnSelected");
-        regBtn.getStyleClass().add("roundTR");
-        regBtn.getStyleClass().add("primaryTextColor");
+            regBtn.getStyleClass().removeAll("regBtn");
+            regBtn.getStyleClass().removeAll("roundTRB");
+            regBtn.getStyleClass().removeAll("secondaryTextColor");
 
-        loginBtn.getStyleClass().removeAll("loginBtnSelected");
-        loginBtn.getStyleClass().removeAll("roundTR");
-        loginBtn.getStyleClass().removeAll("primaryTextColor");
+            regBtn.getStyleClass().add("regBtnSelected");
+            regBtn.getStyleClass().add("roundTR");
+            regBtn.getStyleClass().add("primaryTextColor");
 
-        loginBtn.getStyleClass().add("loginBtn");
-        loginBtn.getStyleClass().add("roundTRL");
-        loginBtn.getStyleClass().add("secondaryTextColor");
+            loginBtn.getStyleClass().removeAll("loginBtnSelected");
+            loginBtn.getStyleClass().removeAll("roundTR");
+            loginBtn.getStyleClass().removeAll("primaryTextColor");
 
+            loginBtn.getStyleClass().add("loginBtn");
+            loginBtn.getStyleClass().add("roundTRL");
+            loginBtn.getStyleClass().add("secondaryTextColor");
 
-        regSelected = true;
+            regSelected = true;
+        }
     }
 
+    //mainly styles
     public void loginBtnSelected() {
-        submitBtn.setText("Bejelentkezés");
+        if (regSelected) {
+            submitBtn.setText("Bejelentkezés");
 
-        nameContainer.setVisible(false);
-        nameContainer.setManaged(false);
-        passwordAgainContainer.setVisible(false);
-        passwordAgainContainer.setManaged(false);
+            nameContainer.setVisible(false);
+            nameContainer.setManaged(false);
+            passwordAgainContainer.setVisible(false);
+            passwordAgainContainer.setManaged(false);
 
-        loginBtn.getStyleClass().removeAll("loginBtn");
-        loginBtn.getStyleClass().removeAll("roundTRL");
-        loginBtn.getStyleClass().removeAll("secondaryTextColor");
+            loginBtn.getStyleClass().removeAll("loginBtn");
+            loginBtn.getStyleClass().removeAll("roundTRL");
+            loginBtn.getStyleClass().removeAll("secondaryTextColor");
 
-        loginBtn.getStyleClass().add("loginBtnSelected");
-        loginBtn.getStyleClass().add("roundTR");
-        loginBtn.getStyleClass().add("primaryTextColor");
+            loginBtn.getStyleClass().add("loginBtnSelected");
+            loginBtn.getStyleClass().add("roundTR");
+            loginBtn.getStyleClass().add("primaryTextColor");
 
-        regBtn.getStyleClass().removeAll("regBtnSelected");
-        regBtn.getStyleClass().removeAll("roundTR");
-        regBtn.getStyleClass().removeAll("primaryTextColor");
+            regBtn.getStyleClass().removeAll("regBtnSelected");
+            regBtn.getStyleClass().removeAll("roundTR");
+            regBtn.getStyleClass().removeAll("primaryTextColor");
 
-        regBtn.getStyleClass().add("regBtn");
-        regBtn.getStyleClass().add("roundTRB");
-        regBtn.getStyleClass().add("secondaryTextColor");
+            regBtn.getStyleClass().add("regBtn");
+            regBtn.getStyleClass().add("roundTRB");
+            regBtn.getStyleClass().add("secondaryTextColor");
 
-        regSelected = false;
+            regSelected = false;
+        }
     }
 
     private void alert(String text) {
